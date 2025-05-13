6ed6065c66cada7d8e1c2f2312ced98f3afa4cbe
Michael Stubbs <michael@nodelondon.com>
2025-05-13 11:44:57 +0100

    Put in place hooks for life cycle management of web socket connections + hook for web socket listener for sending/recieving messages from web socket server - added in new styling using helper for changing colour of work status on state changes for nicer ui

--- src/dashboard/components/WorkStatusCard.tsx
+++ src/dashboard/components/WorkStatusCard.tsx
@@ -2,40 +2,49 @@ import { useSelector, useDispatch } from "react-redux";
 import { DashboardRootState, DashboardDispatch } from "../store";
 import { updateWorkStatus } from "../store/userSlice";
 import { WorkStatus } from "../../shared/types";
+import {
+  COLOUR_TRANSITION_WORK_STATUS,
+  STATE_UPDATE_WORK_AVAILABILITY,
+  STATUS_LABELS,
+  STATUS_OPTIONS_ARRAY
+} from "../../constants";
+import { sendStateUpdateToWebSocket } from "../../helpers/sendStateUpdateToWebSocket";
+import { useWebSocketListener } from "../../hooks/useWebSocketListener";
+import { DropDown } from "./DropDown";
+import { getOptionClass } from "../../helpers/getOptionClass";
+import { useManageWebSocketLifecycle } from "../../hooks/useCleanupWebSocket";
 
-export const WorkStatusCard = ({ className = "" }: { className?: string }) => {
+export const WorkStatusCard = ({ webSocket, className = "" }: { webSocket: WebSocket; className?: string }) => {
+  console.log("WebSocket in WorkStatusCard:", webSocket);
   const { profile } = useSelector((state: DashboardRootState) => state.user);
   const dispatch = useDispatch<DashboardDispatch>();
 
-  const statusLabels: Record<WorkStatus, string> = {
-    looking: "Currently looking for work",
-    passive: "Passively looking for work",
-    not_looking: "Don't want to hear about work",
-  };
+  useManageWebSocketLifecycle(webSocket);
+  useWebSocketListener(webSocket, dispatch, updateWorkStatus);
 
-  const handleStatusChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    dispatch(updateWorkStatus(e.target.value as WorkStatus));
+  const handleStatusChange = (newStatus: string) => {
+    // const newStatus = e.target.value as WorkStatus;
+    const stateUpdateType = STATE_UPDATE_WORK_AVAILABILITY;
+
+    dispatch(updateWorkStatus(newStatus as WorkStatus));
+
+    sendStateUpdateToWebSocket(stateUpdateType, newStatus, webSocket);
   };
 
   return (
-    <div className={`bg-white rounded-lg shadow-sm p-6 h-full ${className}`}>
-      <h3 className="text-lg font-medium mb-4 pb-3 border-b border-gray-200">
-        Your Work Status
-      </h3>
-      <div className="py-2">
+    <div className={`bg-white rounded-lg shadow-sm p-6 h-full   ${className}`}>
+      <h3 className="text-lg font-medium mb-4 pb-3 border-b border-gray-200">Your Work Status</h3>
+      <div className="py-2 flex flex-col gap-y-4">
         <p>Update your availability for new opportunities:</p>
-        <select
-          value={profile.workStatus}
-          onChange={handleStatusChange}
-          className="w-full p-3 border border-gray-200 rounded-md my-4 text-base"
-        >
-          <option value="looking">Currently looking for work</option>
-          <option value="passive">Passively looking for work</option>
-          <option value="not_looking">Don't want to hear about work</option>
-        </select>
-        <p className="mt-4 text-gray-500">
+        <DropDown selectValue={profile.workStatus} onChange={handleStatusChange} options={STATUS_OPTIONS_ARRAY} />
+        <p className=" text-gray-500">
           Your current status:{" "}
-          <strong>{statusLabels[profile.workStatus]}</strong>
+          <strong
+            className={`underline decoration-${getOptionClass(
+              profile.workStatus
+            )} underline-offset-4 ${COLOUR_TRANSITION_WORK_STATUS} ${getOptionClass(profile.workStatus)}`}>
+            {STATUS_LABELS[profile.workStatus]}
+          </strong>
         </p>
       </div>
     </div>
