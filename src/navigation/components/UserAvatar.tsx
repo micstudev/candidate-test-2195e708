52b1c60996f5937c6fa1ef1ba51e09cc1c34d0f7
Michael Stubbs <michael@nodelondon.com>
2025-05-13 11:45:48 +0100

    Put in place hooks for life cycle of web socket plus sending/recieving messages, Updated styling using helper to match WorkStatusCard in terms of new UI and colour changes with state

--- src/navigation/components/UserAvatar.tsx
+++ src/navigation/components/UserAvatar.tsx
@@ -3,68 +3,54 @@ import { NavRootState, NavDispatch } from "../store";
 import { updateWorkStatus } from "../store/userSlice";
 import { WorkStatus } from "../../shared/types";
 import { useState } from "react";
+import { sendStateUpdateToWebSocket } from "../../helpers/sendStateUpdateToWebSocket";
+import {
+  COLOUR_TRANSITION_WORK_STATUS,
+  SHORT_DURATION_ANIMATION_TIME,
+  STATE_UPDATE_WORK_AVAILABILITY,
+  STATUS_LABELS
+} from "../../constants";
+import { useWebSocketListener } from "../../hooks/useWebSocketListener";
+import { WorkStatusDropdown } from "../../dashboard/components/WorkStatusDropdown";
+import { getOptionClass } from "../../helpers/getOptionClass";
+import { useManageWebSocketLifecycle } from "../../hooks/useCleanupWebSocket";
 
-export const UserAvatar = () => {
+export const UserAvatar = ({ webSocket }: { webSocket: WebSocket }) => {
+  console.log("WebSocket in UserAvatar:", webSocket);
   const { profile } = useSelector((state: NavRootState) => state.user);
   const dispatch = useDispatch<NavDispatch>();
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
-  const statusLabels: Record<WorkStatus, string> = {
-    looking: "Currently looking for work",
-    passive: "Passively looking for work",
-    not_looking: "Don't want to hear about work",
-  };
+  useManageWebSocketLifecycle(webSocket);
+  useWebSocketListener(webSocket, dispatch, updateWorkStatus);
 
   const handleStatusChange = (status: WorkStatus) => {
+    const stateUpdateType = STATE_UPDATE_WORK_AVAILABILITY;
     dispatch(updateWorkStatus(status));
+    sendStateUpdateToWebSocket(stateUpdateType, status, webSocket);
     setDropdownOpen(false);
   };
 
   return (
-    <div className="relative">
+    <div className="relative ">
       <div
-        className="flex items-center gap-3 cursor-pointer"
-        onClick={() => setDropdownOpen(!dropdownOpen)}
-      >
-        <img
-          src={profile.avatar}
-          alt={profile.name}
-          className="w-10 h-10 rounded-full"
-        />
+        className={`flex items-center gap-3 cursor-pointer transition-all ${SHORT_DURATION_ANIMATION_TIME} hover:scale-105`}
+        onClick={() => setDropdownOpen(!dropdownOpen)}>
+        <img src={profile.avatar} alt={profile.name} className="w-10 h-10 rounded-full shadow-sm  hover:shadow-md" />
         <div className="flex flex-col">
-          <span className="font-medium text-sm">{profile.name}</span>
-          <span className="text-xs text-gray-600">
-            {statusLabels[profile.workStatus]}
+          <span className={`font-medium transition-all ${SHORT_DURATION_ANIMATION_TIME} text-sm`}>{profile.name}</span>
+          <span className={`text-xs ${COLOUR_TRANSITION_WORK_STATUS} ${getOptionClass(profile.workStatus)}`}>
+            {STATUS_LABELS[profile.workStatus]}
           </span>
         </div>
       </div>
 
       {dropdownOpen && (
-        <div className="absolute top-full mt-2 right-0 bg-white shadow-lg rounded-md p-4 w-64 z-10 border border-gray-200">
-          <h4 className="text-sm font-medium text-gray-700 mb-2">
-            Update your work status:
-          </h4>
-          <ul className="space-y-2">
-            <li
-              onClick={() => handleStatusChange("looking")}
-              className="text-sm py-1.5 px-2 hover:bg-gray-100 rounded cursor-pointer"
-            >
-              Currently looking for work
-            </li>
-            <li
-              onClick={() => handleStatusChange("passive")}
-              className="text-sm py-1.5 px-2 hover:bg-gray-100 rounded cursor-pointer"
-            >
-              Passively looking for work
-            </li>
-            <li
-              onClick={() => handleStatusChange("not_looking")}
-              className="text-sm py-1.5 px-2 hover:bg-gray-100 rounded cursor-pointer"
-            >
-              Don't want to hear about work
-            </li>
-          </ul>
-        </div>
+        <WorkStatusDropdown
+          onStatusChange={handleStatusChange}
+          webSocket={webSocket}
+          setDropdownOpen={setDropdownOpen}
+        />
       )}
     </div>
   );
